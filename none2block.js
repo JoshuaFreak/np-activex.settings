7ff36d29f0d39d2de9ac99a1e90bd48912973812
qiuc12 <qiuc12@gmail.com>
2012-02-26 21:35:16 +0800

    Use integrated page-checking.

--- none2block.js
+++ none2block.js
@@ -1,17 +1,1 @@
-(function() {
-  function reload() {
-    var objs = document.querySelectorAll('object');
-    for (var i = 0; i < objs.length; ++i) {
-      if (getComputedStyle(objs[i]).display == 'none') {
-        objs[i].style.display = 'block';
-        objs[i].style.width = "0px";
-        objs[i].style.height = "0px";
-      }
-    }
-  }
-  if (document.readyState == 'complete') {
-    reload();
-  } else {
-    window.addEventListener('load', reload, false);
-  }
-})();
+scriptConfig.none2block = true;
